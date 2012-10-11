de44b653aa7f8315291d5f6b84d0e0335bbb258a
Ruben Nodeland <ruben.nodeland@gmail.com>
2012-10-11 13:37:32 +0200

    ;

--- pg560 v1/src/main/java/no/nith/pg560/interfaces/web/SearchTechnologyController.java
+++ pg560 v1/src/main/java/no/nith/pg560/interfaces/web/SearchTechnologyController.java
@@ -1,5 +1,16 @@
 package no.nith.pg560.interfaces.web;
 
+
+import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
 public class SearchTechnologyController {
 
+	@Inject
+	private Conversation conversation;
+	
+	
 }
